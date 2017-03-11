e5594d4321b5766d7954b910036b0b83832916c2
salierri <g.gergely314@gmail.com>
2017-03-11 14:19:03 +0100

    Simplified app.js of messageboard

--- 2017-tavasz/node-workshop/messageboard/app.js
+++ 2017-tavasz/node-workshop/messageboard/app.js
@@ -13,8 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -26,17 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',routes);
 
-
-//
-//app.use('/', function (req, res, next) {
-//    res.sendfile(__dirname + '/public/board.html');
-//},routes);
-//
-//app.use('/login', function (req, res, next) {
-//    res.sendfile(__dirname + '/public/login.html');
-//});
-
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
@@ -46,26 +33,14 @@ app.use(function (req, res, next) {
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    response = { message: err.message };
+    if (app.get('env') === 'development') {
+        response.error = err;
+    }
+    console.log(response);
+    res.render('error', response);
 });
 
 
