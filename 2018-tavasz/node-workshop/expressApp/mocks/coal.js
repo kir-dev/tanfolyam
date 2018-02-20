902fa336b2da88cde31a4d611a1370b6fe4c3c8f
fenyopeti <petyu8@msn.com>
2018-02-20 16:48:14 +0100

    Node workshop project

---
+++ 2018-tavasz/node-workshop/expressApp/mocks/coal.js
@@ -0,0 +1,12 @@
+module.exports = [{
+    owner: "Jani",
+    roomNumber: 1020,
+    count: 11,
+    price: 300
+},
+{
+    owner: "Fenyi",
+    roomNumber: 1605,
+    count: 15,
+    price: 250
+}];
