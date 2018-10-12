3fe6b2bb3573509cc3672026c8adab4e49b77af3
kiralybalint <kiraly.balint@simonyi.bme.hu>
2018-10-12 21:58:40 +0200

    4. feladat  - Nested resources

--- 2018-osz/rails-workshop/config/routes.rb
+++ 2018-osz/rails-workshop/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :memberships
-  resources :groups
+  resources :groups do
+    resources :memberships
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
