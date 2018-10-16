efbbe35f94e0a4418db35f593bd917861ec2f082
kiralybalint <kiraly.balint@simonyi.bme.hu>
2018-10-16 15:41:26 +0200

    7. feladat  - User can join group

--- 2018-osz/rails-workshop/app/models/group.rb
+++ 2018-osz/rails-workshop/app/models/group.rb
@@ -1,5 +1,15 @@
 class Group < ApplicationRecord
+  has_many :memberships
+
   def join!(user)
     Membership.create(group: self, user: user, admin: false)
+  end
+
+  def can_join?(user)
+      user && !member?(user)
+  end
+
+  def member?(user)
+    memberships.find { |m| m.user == user }
   end
 end
