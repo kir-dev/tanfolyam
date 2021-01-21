0cc663783e3040d17916bd147912ac77b6ae9c56
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-01-21 21:49:07 +0100

    implement js

--- 2021-tavasz/web-alapok/live-poll/java-script/script.js
+++ 2021-tavasz/web-alapok/live-poll/java-script/script.js
@@ -1,47 +1,100 @@
 
-let question = document.getElementById("question")
+let questionInput = document.getElementById("question")
 let answerContainer = document.getElementById("answer-container")
 let content = document.getElementById("content")
-let createForm = document.getElementById("create-form")
+let createForm = document.getElementById("question-form")
+let answerCnt = 0;
 
 document.getElementById("add-button").addEventListener("click", addNewAswer)
 function addNewAswer(){
-    let answerText = document.getElementById("new-answer").value
+    let newAnswerInput = document.getElementById("new-answer")
+    let answerText = newAnswerInput.value
 
     let newAnswer = document.createElement("div")
     newAnswer.innerText = answerText
 
     answerContainer.appendChild(newAnswer)
+    newAnswerInput.value=""
 }
 
-document.getElementById("save-button").addEventListener("click", saveAnswer)
-function saveAnswer(){
+function getQuestions(){
+    return JSON.parse(localStorage.getItem("questions")) || []
+}
+function saveQuestion(question){
+    let questions = getQuestions()
+    questions.push(question)
+    localStorage.setItem("questions", JSON.stringify(questions))
+}
+loadQuestions()
+function loadQuestions(){
+    let questions = getQuestions()
+    for (let index = 0; index < questions.length; index++) {
+        let question = questions[index];
+        renderQuestion(question)
+    }
+}
+
+document.getElementById("save-button").addEventListener("click", createQuestion)
+function createQuestion(){
+    let question = {id: answerCnt,text: questionInput.value, answers:[]}
+
+    const answers = answerContainer.children;
+    for (let index = 0; index < answers.length; index++) {
+        question.answers.push(answers[index].innerText)
+    }
+
+    saveQuestion(question)
+    renderQuestion(question)
+    answerCnt+=1
+    
+    questionInput.value = ""
+    answerContainer.innerHTML=""
+}
+
+function renderQuestion(question){
     const section = document.createElement("div")
     section.className = "section"
     content.insertBefore(section, createForm )
 
     const header = document.createElement("h3")
-    header.innerText = question.value
+    header.innerText = question.text
     section.appendChild(header)
 
     const form = document.createElement("form")
     section.appendChild(form)
 
-    const answers = answerContainer.children;
+    let answers = question.answers
     for (let index = 0; index < answers.length; index++) {
-        const element = answers[index]
+        const answer = answers[index]
 
         let checkbox = document.createElement("input")
         checkbox.type="radio"
+        checkbox.name="answer-"+answerCnt
         form.appendChild(checkbox)
 
         let label = document.createElement("label")
-        label.innerText = element.innerText
+        label.innerText = answer
         form.appendChild(label)
         
         let lineBreak = document.createElement("br")
         form.appendChild(lineBreak)
     }
+}
 
+let openButton = document.getElementById("open-button")
+let questionForm = document.getElementById("question-form")
+
+document.getElementById("cancel-button").addEventListener("click", cancelQuestion)
+function cancelQuestion(){
+    questionForm.style.display="none"
+
+    openButton.style.display="inline"
+}
+
+document.getElementById("open-button").addEventListener("click",openQuestionForm)
+function openQuestionForm(){
+    questionForm.style.display="block"
+
+    openButton.style.display="none"
 }
 
