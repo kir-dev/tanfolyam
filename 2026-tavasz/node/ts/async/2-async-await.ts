773fb2222d597a7f149cf7ae4c8e679a109b1d97
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2026-02-17 16:59:00 +0100

    Add express and typescript examples

---
+++ 2026-tavasz/node/ts/async/2-async-await.ts
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch';
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+export const fetchUser = async (userId: string): Promise<User> => {
+  return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`).then(
+    (res) => res.json(),
+  ) as Promise<User>;
+};
+
+// Tegyük fel, hogy a Facebook backendjén dolgozunk, a barátnak jelölés funkciót szeretnénk implementálni
+// Azt a kis feladatot kaptuk, hogy írjunk egy segédfüggvényt,
+// ami két felhasználó adait lekéri az adatbázisból, és egy tömbben visszaküldi.
+
+// Promise
+const checkFriendsPromise = () => {
+  let user1: User;
+  return fetchUser('1')
+    .then((value) => {
+      user1 = value;
+      return fetchUser('2');
+    })
+    .then((user2) => [user2, user1]);
+};
+
+// Async + Await
+export const checkFriendsAwait = async () => {
+  try {
+    const user1 = await fetchUser('1');
+    const user2 = await fetchUser('2');
+    return [user1, user2];
+  } catch {}
+};
+
+// checkFriendsPromise().then((a) => console.log(a));
+// checkFriendsAwait().then(console.log);
---
+++ 2026-tavasz/node/ts/basics/basics.ts
@@ -0,0 +1,38 @@
+// interfaces
+interface Kitty {
+  name: string;
+  age?: number;
+  children: Kitty[] | string[];
+}
+
+const array: Kitty[] = [
+  {
+    name: 'Cirmos',
+    age: 1,
+    children: ['Scruffles', 'Snowballs II'],
+  },
+  { name: 'Foltos', children: [] },
+];
+
+// functions
+function logFunction(element: Kitty, index: number): void {
+  console.log(`${index} - ${element.name}: ${element.age ?? 'Újszülött'}.`);
+}
+
+const logArrowFunction = (element: Kitty, index: number): void => {
+  console.log(
+    `${index} - ${element.name}: ${
+      element.age ? 'Nem újszülött' : 'Újszülött'
+    }.`,
+  );
+};
+
+console.log('Old way:');
+for (let i = 0; i < array.length; ++i) {
+  logFunction(array[i], i);
+}
+
+console.log('\nArrow function way:');
+array.forEach((element, index) => {
+  logArrowFunction(element, index);
+});
